c1972b8e60dcce02b8a77474f692c817c9f62860
SyedAlam-coder <alamkhan226312@gmail.com>
2025-07-17 04:52:10 +0500

    new changes dark moode

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { movies } from './data'
 
 const App = () => {
   const [movie, setmovies] = useState(movies)
+  const [toggle,setToggle]=useState(false)
 
   const FilterbyCategory = (cat) => {
     if (cat === "All") {
@@ -16,11 +17,17 @@ const App = () => {
   }
 
   return (
-    <div className="main-container w-screen min-h-screen bg-black">
+    <div className={`main-container w-screen min-h-screen ${toggle?"bg-black":"bg-white"} ${toggle?"text-white":"text-black"}`}>
       <Tabs FilterbyCategory={FilterbyCategory} />
+
+   {/* toggle dark mode */}
+
+      <button className={`  text-sm font-bold w-[40px] h-[40px] rounded-full border-none ml-3 animate-bounce duration-300  ${toggle?"bg-white":"bg-black"} ${toggle?"text-black":"text-white"} `} onClick={()=>{setToggle(!toggle)}}>{toggle?"Light":"Dark"}</button>
+
+      {/* map through all movie */}
       <div className="max-w-[1200px] w-full mx-auto flex flex-wrap justify-center py-5 gap-5">
         {movie.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
+          <MovieCard key={movie.id} movie={movie} toggle={toggle} />
         ))}
       </div>
     </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie,toggle }) => {
   return (
     <div className=" w-[350px] hover:transform hover:scale-[1.05] duration-300 md:w-52 overflow-hidden flex flex-col items-center p-2 border-2 rounded  border-cyan-700">
       <div className={`image w-full md:h-52    rounded-md overflow-hidden`}>
         <img className="object-cover w-full h-full rounded" src={movie.poster_path} alt="" />
       </div>
-      <div className="title font-bold text-white tracking-wide text-center mb-2">{movie.title}</div>
+      <div className={`title font-bold ${toggle?"text-white":"text-black"} tracking-wide text-center mb-2`}>{movie.title}</div>
       <div className="date text-gray-500 text-sm text-center">{movie.release_date}</div>
     </div>
   )
